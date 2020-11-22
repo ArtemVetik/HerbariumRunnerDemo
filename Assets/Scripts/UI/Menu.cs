74c2713ff744c20dff70e1e63db42bc4a353c468
ArtemVetik <av.unitygames@gmail.com>
2020-11-22 12:53:21 +0300

    Refactoring

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -3,22 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private GameBalance _balance;
-    [SerializeField] private SkinDataBase _dateBase;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F11))
-            _balance.AddBalance(100f);
-
-        if (Input.GetKeyDown(KeyCode.F10))
-        {
-            PlayerPrefs.DeleteAll();
-            //SkinSaved skinSaved = new SkinSaved(_dateBase);
-            //skinSaved.Save(new JsonSaveLoad());
-        }
-    }
-
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
